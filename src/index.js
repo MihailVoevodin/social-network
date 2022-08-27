668ce9aa802b438848e9e9c2db665b97d87b32a1
Mikhail Voevodin <72670840+MihailVoevodin@users.noreply.github.com>
2022-08-27 11:22:41 +0300

    Add BG

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import store from './redux/redux-store';
 import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
